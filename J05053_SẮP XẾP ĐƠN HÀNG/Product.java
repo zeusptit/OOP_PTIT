203fc555ac9e287362cddb6c2d254419a294db3a
unknown <cuongtranphu1@gmail.com>
2023-11-17 02:30:15 +0700

    Add files via upload

---
+++ J05053_SẮP XẾP ĐƠN HÀNG/Product.java
@@ -0,0 +1,32 @@
+    public class Product implements Comparable<Product>{
+    private String productName;
+    private String productID;
+    private int price;
+    private int amount;
+    private int discount;
+
+    public Product(String productName, String productID, int price, int amount) {
+        this.productName = productName;
+        this.productID = productID;
+        this.price = price;
+        this.amount = amount;
+        if(this.productID.charAt(productID.length() - 1) == '1')this.discount = 5;
+        else this.discount = 3;
+    }
+    public String getOrder(){
+        return productID.substring(1, 4);
+    }
+    public int getDiscount(){
+        return (int) Math.ceil(price * amount * discount / 10);
+    }
+
+    @Override
+    public String toString() {
+        return productName + " " + productID + " " + getOrder() + " " + getDiscount() + " " + (price * amount - getDiscount());
+    }
+
+    @Override
+    public int compareTo(Product o) {
+        return Integer.parseInt(getOrder()) - Integer.parseInt(o.getOrder());
+    }
+}
--- JKT013_SỐ LỘC PHÁT/Main.java
+++ JKT013_SỐ LỘC PHÁT/Main.java
@@ -4,32 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.next();
-        Stack<Character> stack1 = new Stack<>(), stack2 = new Stack<>();
-        for(char x : s.toCharArray()){
-            if(x == '<'){
-                if(!stack1.isEmpty()) {
-                    stack2.push(stack1.peek());
-                    stack1.pop();
+        int test = scanner.nextInt();
+        while (test-- > 0) {
+            int n = scanner.nextInt();
+            Queue<String> queue = new ArrayDeque<>();
+            queue.add("6"); queue.add("8");
+            ArrayList<String> list = new ArrayList<>();
+            while(true){
+                String temp = queue.peek(); queue.remove();
+                if(temp.length() > n){
+                    break;
+                }else{
+                    list.add(temp);
                 }
-            }else if(x == '>'){
-                if(!stack2.isEmpty()) {
-                    stack1.push(stack2.peek());
-                    stack2.pop();
-                }
-            }else if(x == '-') {
-                if (!stack1.isEmpty())
-                    stack1.pop();
-            }else
-                stack1.push(x);
-        }
-        while(!stack1.isEmpty()){
-            stack2.push(stack1.peek());
-            stack1.pop();
-        }
-        while(!stack2.isEmpty()){
-            System.out.print(stack2.peek());
-            stack2.pop();
+                queue.add(temp + "6");
+                queue.add(temp + "8");
+            }
+            Collections.reverse(list);
+            System.out.println(list.size());
+            for(String x : list){
+                System.out.print(x + " ");
+            }
+            System.out.println();
         }
     }
 }
